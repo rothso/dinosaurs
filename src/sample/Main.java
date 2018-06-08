ec1483656a1c3473e2a0bb563ed6e1ad97ea7732
Rothanak So <rothanak.hello@gmail.com>
2018-06-07 21:11:58 -0400

    Begin sketching out the "jump" animation handlers

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,14 +2,18 @@ package sample;
 
 import java.io.File;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
@@ -25,14 +29,25 @@ public class Main extends Application {
     Image dino = new Image(file.toURI().toString());
     ImageView imageView = new ImageView(dino);
 
-    // Bind spacebar to jump
+    // Bind spacebar to jump (tell the image it needs to go up)
     EventHandler<KeyEvent> doJump = new EventHandler<KeyEvent>() {
+      int dy = 0; // TODO move to the outside so the backgroundLoop can access it
+
       @Override
       public void handle(KeyEvent event) {
-        // TODO!
+        if (event.getCode() == KeyCode.SPACE) {
+          imageView.setTranslateY(dy); // TODO actually think about this
+        }
       }
     };
     imageView.setOnKeyPressed(doJump);
+
+    // The background loop will emulate gravity (what goes up must go down)
+    KeyFrame keyFrame = new KeyFrame(Duration.millis(10), event -> {
+      double currentY = imageView.getY();
+      // TODO update the new y-position of the image based on dy
+    });
+    Timeline backgroundLoop = new Timeline(); // TODO configure the timeline and start() it
 
     // Add the gif to a pane
     root.getChildren().add(imageView);
